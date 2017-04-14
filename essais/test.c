d1747130387faaca56347870800c9a7adba21fd5
Sarah FRANC <sfranc@e3r10p18.42.fr>
2017-04-14 19:27:12 +0200

    wip

--- essais/test.c
+++ essais/test.c
@@ -6,7 +6,7 @@
 /*   By: sfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 16:53:20 by sfranc            #+#    #+#             */
-/*   Updated: 2017/04/12 16:21:27 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/04/14 11:30:05 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int ac, char **av)
 {
 	pid_t	father;
 	int		i;
+	int		ret;
 
 	i = 0;
 	printf("father ID : %d,\t %d\n", getpid(), i++);
@@ -25,18 +26,15 @@ int	main(int ac, char **av)
 	printf("father ID : %d,\t %d\n", getpid(), i++);
 	if (father > 0) // je suis dans le pere
 	{
-		father = fork();
-		wait(0);
-		printf("father ID : %d,\t %d\n", getpid(), i++);
 		printf("if > 0, father ID : %d,\t %d\n", getpid(), i++);
 		wait(0); // wait prend un pointeur sur int*, em mettant rien en fait c'est 0 donc NULL.
-		printf("PAPA\n");
-		
+		printf("PAPA\n");	
 	}
 	if (father == 0) // c'est le fils
 	{
 		printf("if == 0, father ID : %d,\t %d\n", getpid(), i++);
-		execve("/bin/ls", av, NULL);
+		ret = execve("/bin/ls", av, NULL);
+		printf("%d\n", ret);
 	}
 	return (0);
 }
