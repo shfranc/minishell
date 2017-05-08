765f32982bce6a877396b0b2e0a20cae965e4539
Sarah FRANC <sfranc@e2r5p18.42.fr>
2017-05-08 13:31:33 +0200

    suite signaux

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,14 +6,18 @@
 /*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 12:21:08 by sfranc            #+#    #+#             */
-/*   Updated: 2017/05/08 10:54:30 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/05/08 13:30:47 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <errno.h>
 
 void		ft_handler_father(int sig)
 {
+	ft_putstr("father");
+	// ft_putendl(getenv("test="));
+
 	if (sig == SIGINT)
 	{
 		write(1, "\n", 1);
@@ -23,10 +27,26 @@ void		ft_handler_father(int sig)
 
 void		ft_handler_child(int sig)
 {
+	// ft_putstr("child");
+	// ft_putendl(getenv("SHLVL="));
+
 	if (sig == SIGINT)
 	{
-		if (wait(0))
+		if (kill(SIGCHLD, SIGINT) == -1)
+		{
+			ft_putstr("la");
 			write(1, "\n", 1);
-		kill(SIGCHLD, SIGINT);
+			// kill(SIGCHLD, SIGINT);
+		}
+
+		// if (wait(0) != -1)
+		// {
+		// 	write(1, "\n", 1);
+		// 	kill(SIGCHLD, SIGINT);
+		// }
+		// else
+		// {
+		// 	ft_putendl(strerror(errno));
+		// }
 	}
 }
